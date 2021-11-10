d200c7ba9d1c1abb696881d20baadcea9775d3eb
Namratha490 <68885903+Namratha490@users.noreply.github.com>
2021-11-10 15:56:21 +0530

    Add files via upload

---
+++ CAT3/details_entry.php
@@ -0,0 +1,37 @@
+<?php
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="christ";
+$conn=mysqli_connect($server_name, $username, $password, $database_name)
+;
+//now check the connection
+if(!$conn)
+{
+die("Connection Failed: " mysqli_connect_error());
+}
+if(isset($_POST['save']))
+{
+$first_name $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$gender $_POST['gender'];
+$email $_POST['email'];
+$phone $_POST['phone'];
+$sql_query = "INSERT INTO entry_details
+(first_name, last_name, gender, email, mobile)
+VALUES
+('$first_name', '$last_name', '$gender', '$email', '$phone')";
+if (mysqli_query($conn, $sql_query))
+{
+echo "New Details Entry inserted successfully !";
+}
+else
+{
+II TI
+$sql .
+mysqli_error($conn);
+echo "Error:
+}
+mysqli_close($conn);
+}
+?>
